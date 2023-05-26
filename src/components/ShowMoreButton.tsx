fdbaa91cf4fd220275f77c1b409a4995e7b053f5
noamtayri <noamtayri1990@gmail.com>
2023-05-26 17:35:25 +0300

    infinite loader

--- src/components/ShowMoreButton.tsx
+++ src/components/ShowMoreButton.tsx
@@ -1,3 +1,4 @@
+import { useRef, useEffect } from 'react';
 import { loadMoreFollowers } from '../features/followers/followersSlice';
 import { setPage } from '../features/pagination/paginationSlice';
 import { useAppSelector, useAppDispatch } from '../app/hooks';
@@ -5,18 +6,39 @@ import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 
 export function ShowMoreButton() {
+  const buttonRef = useRef(null);
   const page = useAppSelector((state) => state.pagination.page);
   const dispatch = useAppDispatch();
 
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) {
+          loadMoreFn();
+        }
+      },
+      { threshold: 1 }
+    );
+
+    if (buttonRef.current) {
+      observer.observe(buttonRef.current);
+    }
+
+    return () => {
+      if (buttonRef.current) {
+        observer.unobserve(buttonRef.current);
+      }
+    };
+  }, [buttonRef]);
+
+  const loadMoreFn = () => {
+    dispatch(loadMoreFollowers(page + 1));
+    dispatch(setPage(page + 1));
+  };
+
   return (
     <Box>
-      <Button
-        onClick={() => {
-          dispatch(loadMoreFollowers(page + 1));
-          dispatch(setPage(page + 1));
-        }}
-        variant="text"
-      >
+      <Button ref={buttonRef} onClick={loadMoreFn} variant="text">
         Show more
       </Button>
     </Box>
